193aefeaa14793f4a918df888b7dcf9a5ba4dc4b
Bill Rousseau <bill.rousseau@outlook.com>
2017-12-26 14:12:40 +0100

    balance vol and skip-turn behaviors

--- src/com/parisjeux/beans/Mauvais.java
+++ src/com/parisjeux/beans/Mauvais.java
@@ -15,17 +15,20 @@ public class Mauvais extends Personnage {
          */
         // Ici je cherche à déterminer si l'instance courante est de type Necromantien.
         // TODO: utiliser instanceof pour être plus scalable
-        Boolean skipTurn = (this.phy == 22 && this.vol == 15) ? this.randomizeAttack(33) : this.randomizeAttack(10);
+        if (this.getPm() < 100) {
+        	Boolean skipTurn = (this.phy == 22 && this.vol == 15) ? this.randomizeAttack(33) : this.randomizeAttack(10);
 
-        if (skipTurn) {
-            return this.passerTour();
+            if (skipTurn) {
+                return this.passerTour();
+            }
         }
+        
         /**
          * A 50 pm ou moins, le m�chant lance toujours une attaque PHY.
          */
-        else if (!this.isAtkSpePossible() && !this.isAtkUltPossible()) {
+        if (!this.isAtkSpePossible() && !this.isAtkUltPossible()) {
             return this.atkPhy(p);
-        }
+        } 
         /**
          * Entre 50 et 74 pm, le m�chant a 20% de chances de lancer une attaque SPE.
          */
--- src/com/parisjeux/beans/Necromantien.java
+++ src/com/parisjeux/beans/Necromantien.java
@@ -8,8 +8,8 @@ public class Necromantien extends Mauvais {
 		super();
 		this.setNom("Death Notes");
 		this.pv = ThreadLocalRandom.current().nextInt( 400 , 500 + 1 ); 
-		this.pm = 100;
-		this.phy = 22;
+		this.pm = 0;
+		this.phy = 3;
 		this.spe = ThreadLocalRandom.current().nextInt( 70, 110 + 1 );
 		this.ult = ThreadLocalRandom.current().nextInt( 150 , 200 + 1 );
 		this.vol = 15;
